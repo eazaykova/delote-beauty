f149656c4ac503652ccb37d0af30a1d923e0b140
eazaykova <lena.zaykova.95@gmail.com>
2023-08-18 20:08:09 +0700

    Improved Header

--- src/components/layout/Header/menu/Menu.jsx
+++ src/components/layout/Header/menu/Menu.jsx
@@ -1,38 +1,62 @@
 import styles from "./Menu.module.scss";
 import NavRef from "../../../ui/NavRef/NavRef";
-import { menu } from "./menu.data";
+
 import DetailInfo from "./detailInfo/DetailInfo";
 import { useState } from "react";
 import Button from "../../../ui/button/Button";
+import { menu } from "../../../../data/menu.data";
+import Hamburger from "./hamburger/Hamburger";
 
 const Menu = () => {
   const [activeDetailInfo, setActiveDetailInfo] = useState(true);
+  const [activeHamburger, setActiveHamburger] = useState(false);
 
   return (
-    <nav>
-      <ul className={styles.menuList}>
-        <a href="https://vk.com/" target="_blank">
-          <img src="/icons/vk.svg" alt="VK" />
-        </a>
-        {menu.map((item, index) =>
-          item.link ? (
-            <li key={`_menu_${index}`}>
-              <NavRef href={item.link}>{item.title}</NavRef>
-            </li>
-          ) : (
-            <img
-              key={`_menu_${index}`}
-              src="/icons/logo.svg"
-              alt="Логотип Delote Beauty"
-            />
-          )
-        )}
-        <Button clickHandler={() => setActiveDetailInfo(true)}>
-          <img src="/icons/info.svg" alt="Information" />
-        </Button>
-      </ul>
+    <>
+      <nav className={styles.menuFull}>
+        <ul className={styles.menuList}>
+          <a href="https://vk.com/" target="_blank">
+            <img src="/icons/vk.svg" alt="VK" />
+          </a>
+          {menu.map((item, index) =>
+            item.link ? (
+              <li key={`_menu_${index}`}>
+                <NavRef href={item.link}>{item.title}</NavRef>
+              </li>
+            ) : (
+              <img
+                key={`_menu_${index}`}
+                src="/icons/logo.svg"
+                alt="Логотип Delote Beauty"
+              />
+            )
+          )}
+          <Button clickHandler={() => setActiveDetailInfo(true)}>
+            <img src="/icons/info.svg" alt="Information" />
+          </Button>
+        </ul>
+      </nav>
+
+      <nav className={styles.menuMobile}>
+        <ul className={styles.menuList}>
+          <img
+            src="/icons/logo.svg"
+            alt="Логотип Delote Beauty"
+            style={{ marginLeft: "10px" }}
+          />
+          <div>
+            <Button clickHandler={() => setActiveHamburger(true)}>
+              <img src="/icons/menu.svg" alt="Hamburger" />
+            </Button>
+            <Button clickHandler={() => setActiveDetailInfo(true)}>
+              <img src="/icons/info.svg" alt="Information" />
+            </Button>
+          </div>
+        </ul>
+      </nav>
       <DetailInfo active={activeDetailInfo} setActive={setActiveDetailInfo} />
-    </nav>
+      <Hamburger active={activeHamburger} setActive={setActiveHamburger} />
+    </>
   );
 };
 
