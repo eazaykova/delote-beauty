49d750dcaab7430ed7f0d910448c9a249cbf51b9
eazaykova <lena.zaykova.95@gmail.com>
2023-08-18 20:05:54 +0700

    Added styles and elements to Home

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,7 +1,23 @@
+import styles from "./Home.module.scss";
 import Layout from "../../layout/Layout";
 
 const Home = () => {
-  return <Layout>Тест</Layout>;
+  return (
+    <Layout>
+      <div className={styles.bg}></div>
+      <section className={styles.preview}>
+        <h1 className={styles.title}>
+          Салон красоты <br />
+          «Delote-Beauty» <br />
+          на Крестовском
+        </h1>
+        <div className={styles.mouseDown}>
+          <img src="/icons/mouse-down.svg" alt="Mouse Down" />
+          <span>Прокрутите вниз</span>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export default Home;
