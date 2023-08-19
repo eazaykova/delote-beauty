7dac9d5ce961b2d16d21f1fdaa4801d259f31f49
eazaykova <lena.zaykova.95@gmail.com>
2023-08-19 21:07:28 +0700

    Created cosmetics.data.js and services.data.js

---
+++ src/data/cosmetics.data.js
@@ -0,0 +1,18 @@
+export const cosmetics = [
+  {
+    name: "KEVIN.MURPHY",
+    icon: "/icons/cosmetics/cosmetics_1.svg",
+  },
+  {
+    name: "ORIBE",
+    icon: "/icons/cosmetics/cosmetics_2.svg",
+  },
+  {
+    name: "ALTERNA",
+    icon: "/icons/cosmetics/cosmetics_3.svg",
+  },
+  {
+    name: "ALDO COPPOLA",
+    icon: "/icons/cosmetics/cosmetics_4.svg",
+  },
+];
---
+++ src/data/services.data.js
@@ -0,0 +1,26 @@
+export const services = [
+  {
+    title: "Парикмахерские услуги",
+    img: "/images/services/services_1.jpg",
+  },
+  {
+    title: "Маникюр",
+    img: "/images/services/services_2.jpg",
+  },
+  {
+    title: "Педикюр",
+    img: "/images/services/services_3.jpg",
+  },
+  {
+    title: "Косметология",
+    img: "/images/services/services_4.jpg",
+  },
+  {
+    title: "Эстетист по телу",
+    img: "/images/services/services_5.jpg",
+  },
+  {
+    title: "Визаж",
+    img: "/images/services/services_6.jpg",
+  },
+];
