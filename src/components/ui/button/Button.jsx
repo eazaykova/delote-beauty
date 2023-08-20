72b238a720827c8ae148dbe0bca7c1274b556632
eazaykova <lena.zaykova.95@gmail.com>
2023-08-20 19:01:30 +0700

    Сhanged button

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,8 +1,19 @@
 import styles from "./Button.module.scss";
 
-const Button = ({ children, clickHandler = null }) => {
+const Button = ({ children, clickHandler = null, isUnderline = false }) => {
+  const onClick = (e) => {
+    if (isUnderline) {
+      const activeButton = document.querySelectorAll(".active_button");
+      activeButton.forEach((current) =>
+        current.classList.remove("active_button")
+      );
+      e.target.classList.add("active_button");
+    }
+    clickHandler();
+  };
+
   return (
-    <button className={styles.button} onClick={clickHandler}>
+    <button className={styles.button} onClick={(e) => onClick(e)}>
       {children}
     </button>
   );
