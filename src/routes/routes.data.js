4309455bde2543616aae9f6a08c6615161f4f7ca
eazaykova <lena.zaykova.95@gmail.com>
2023-08-20 20:10:31 +0700

    Сhanged routes.data.js

--- src/routes/routes.data.js
+++ src/routes/routes.data.js
@@ -1,9 +1,15 @@
 import Home from "../components/screens/home/Home";
+import Masters from "../components/screens/masters/Masters";
 
 export const routes = [
   {
     path: "/",
     component: Home,
+    isAuth: false,
+  },
+  {
+    path: "/masters",
+    component: Masters,
     isAuth: false,
   },
 ];
