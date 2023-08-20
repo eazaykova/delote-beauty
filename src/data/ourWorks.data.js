d6e415c4d51d3664afaf8f20650a17a68d83d5b4
eazaykova <lena.zaykova.95@gmail.com>
2023-08-20 18:59:35 +0700

    Created ourWorks.data.js

---
+++ src/data/ourWorks.data.js
@@ -0,0 +1,38 @@
+export const ourWorks = [
+  {
+    type: "makeup",
+    img: "/images/our-works/work_9.jpg",
+  },
+  {
+    type: "makeup",
+    img: "/images/our-works/work_6.jpg",
+  },
+  {
+    type: "manicure",
+    img: "/images/our-works/work_3.jpg",
+  },
+  {
+    type: "makeup",
+    img: "/images/our-works/work_8.jpg",
+  },
+  {
+    type: "hairdresser",
+    img: "/images/our-works/work_5.jpg",
+  },
+  {
+    type: "makeup",
+    img: "/images/our-works/work_2.jpg",
+  },
+  {
+    type: "manicure",
+    img: "/images/our-works/work_7.jpg",
+  },
+  {
+    type: "hairdresser",
+    img: "/images/our-works/work_4.jpg",
+  },
+  {
+    type: "hairdresser",
+    img: "/images/our-works/work_1.jpg",
+  },
+];
