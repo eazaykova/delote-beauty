1f9d84aee3322a03b339dd04af6134fca5b131c2
eazaykova <lena.zaykova.95@gmail.com>
2023-08-20 20:08:42 +0700

    Created masters.data.js

---
+++ src/data/masters.data.js
@@ -0,0 +1,32 @@
+export const masters = [
+  {
+    name: "Еремеева Анна",
+    profession: "Парикмахер",
+    img: "/images/masters/masters_1.jpg",
+  },
+  {
+    name: "Власова Варвара",
+    profession: "Визажист",
+    img: "/images/masters/masters_2.jpg",
+  },
+  {
+    name: "Дмитриева Ева",
+    profession: "Визажист",
+    img: "/images/masters/masters_3.jpg",
+  },
+  {
+    name: "Виноградова Зоя",
+    profession: "Мастер по маникюру",
+    img: "/images/masters/masters_4.jpg",
+  },
+  {
+    name: "Семенова Мария",
+    profession: "Парикмахер",
+    img: "/images/masters/masters_5.jpg",
+  },
+  {
+    name: "Сергеева Дарья",
+    profession: "Визажист",
+    img: "/images/masters/masters_6.jpg",
+  },
+];
