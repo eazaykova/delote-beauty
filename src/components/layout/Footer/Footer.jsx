5bfafd44ddf3ac51e31ec00b9177fd56db559816
eazaykova <lena.zaykova.95@gmail.com>
2023-08-20 19:22:05 +0700

    Сhanged Footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,7 +1,28 @@
+import { inform } from "../../../data/inform.data";
+import TextList from "../../generic/textList/textList";
 import styles from "./Footer.module.scss";
+import stylesTextList from "../../generic/textList/TextList.module.scss";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.topFooter}>
+        <img src="/icons/logo.svg" alt="Логотип Delote Beauty" />
+        {inform.map((item, index) => (
+          <TextList key={item + index} title={item.title} list={item.list} />
+        ))}
+
+        <div>
+          <h3 className={stylesTextList.title}>Мы в VK</h3>
+          <a href="https://vk.com/" target="_blank">
+            <img src="/icons/vk.svg" alt="VK" />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.subFooter}>Copyright © 2017 - 2023</div>
+    </footer>
+  );
 };
 
 export default Footer;
