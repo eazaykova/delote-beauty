5e0a7836898442a048d1a9bd2f8380cac8c18b9f
eazaykova <lena.zaykova.95@gmail.com>
2023-08-22 19:25:46 +0700

    Сhanged src/routes/routes.data.js and menu.data.js

--- src/data/menu.data.js
+++ src/data/menu.data.js
@@ -13,7 +13,7 @@ export const menu = [
   },
   {
     title: "Цены",
-    link: "/rices",
+    link: "/price",
   },
   {
     title: "Контакты",
--- src/routes/routes.data.js
+++ src/routes/routes.data.js
@@ -1,5 +1,6 @@
 import Home from "../components/screens/home/Home";
 import Masters from "../components/screens/masters/Masters";
+import Price from "../components/screens/price/Price";
 
 export const routes = [
   {
@@ -10,6 +11,11 @@ export const routes = [
   {
     path: "/masters",
     component: Masters,
+    isAuth: false,
+  },
+  {
+    path: "/price",
+    component: Price,
     isAuth: false,
   },
 ];
