259a653f24217a6c15ca3fa8271368bc79584c9c
eazaykova <lena.zaykova.95@gmail.com>
2023-08-22 19:24:49 +0700

    Created src/data/price.data.js

---
+++ src/data/price.data.js
@@ -0,0 +1,36 @@
+export const price = [
+  {
+    img: "/images/price/price_1.jpg",
+    name: "Парикмахерские услуги",
+    services: [
+      { title: "Стрижка женская", price: 2500, subTitle: "укладка" },
+      { title: "Оформление чёлки", price: 700, subTitle: "" },
+      { title: "Мужская стрижка", price: 1500, subTitle: "" },
+      { title: "Долговременная укладка", price: 7000, subTitle: "биозавивка" },
+    ],
+  },
+  {
+    img: "/images/price/price_2.jpg",
+    name: "Макияж и брови",
+    services: [
+      {
+        title: "Оформление бровей",
+        price: 1200,
+        subTitle: "коррекция, окрашивание",
+      },
+      { title: "Коррекция бровей для мужчин", price: 1000, subTitle: "" },
+      { title: "Макияж дневной", price: 3000, subTitle: "" },
+      { title: "Макияж вечерний", price: 3500, subTitle: "" },
+    ],
+  },
+  {
+    img: "/images/price/price_3.jpg",
+    name: "Маникюр",
+    services: [
+      { title: "Детский маникюр", price: 800, subTitle: "" },
+      { title: "Маникюр без покрытия", price: 1200, subTitle: "" },
+      { title: "Маникюр с покрытием", price: 3000, subTitle: "дизайн" },
+      { title: "Ремонт одного ногтя", price: 170, subTitle: "" },
+    ],
+  },
+];
